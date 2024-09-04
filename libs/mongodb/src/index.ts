c1fe9ba26b27fd10914f471af412b494d81d107b
Nick Tsitlakidis <nicktsitlakidis@gmail.com>
2024-09-04 16:08:52 +0300

    fix: Event store implementations are exported properly from their modules

--- libs/mongodb/src/index.ts
+++ libs/mongodb/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./lib/event-nest-mongodb.module";
 export * from "./lib/mongodb-module-options";
+export * from "./lib/storage/mongo-event-store";
--- libs/postgresql/src/index.ts
+++ libs/postgresql/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./lib/event-nest-postgresql.module";
 export * from "./lib/postgresql-module-options";
+export * from "./lib/storage/postgresql-event-store";
