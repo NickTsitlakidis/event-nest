c98d40e16ecd48bf1df6e644e41d9085183b7e83
Nick Tsitlakidis <nicktsitlakidis@gmail.com>
2026-05-19 13:22:39 +0300

    Fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -61,7 +61,7 @@ module.exports = [
         rules: {
             "@typescript-eslint/no-empty-function": "off",
             "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-non-null-assertion": "off"
+            "@typescript-eslint/no-non-null-assertion": "off",
             "unicorn/prevent-abbreviations": "off"
         }
     },
